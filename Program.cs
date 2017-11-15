73b6b5c601f9dec216bb7e4619d28e0ed11adcf2
Unknown <eolsurion369@hotmail.com>
2017-11-15 14:00:03 -0800

    C# Exercise 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFoundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Hello " + myName);
+
+        }
+    }
+}
