96711c7c323ea4f7f7b42b67d417169211e43ef1
Unknown <eolsurion369@hotmail.com>
2017-11-15 15:18:25 -0800

    C# Exercise 2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 
+/*
+****************************************************************************************
+    Author:     Robert Fike
+    Date:       11/15/17
+    Project:    C# Sharp Foundations
+****************************************************************************************
+ */
+
 namespace CSharpFoundations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name");
-            string myName = Console.ReadLine();
-            Console.WriteLine("Hello " + myName);
-
+            Console.WriteLine("Please enter a number between 5 and 20");
+            int myNumber = int.Parse(Console.ReadLine());
+            
+            int i = 1;
+            
+            while (i <= myNumber) {
+                if (i % 2 == 0) {
+                    Console.WriteLine(i + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(i + " is odd");
+                }
+                i++;
+            }
         }
     }
 }
