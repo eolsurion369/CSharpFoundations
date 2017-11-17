35c3fbd504d7df3dddcc58ffee8f01436d4b93af
Unknown <eolsurion369@hotmail.com>
2017-11-17 11:10:54 -0800

    C# Exercise 4

--- Program.cs
+++ Program.cs
@@ -15,12 +15,10 @@ namespace CSharpFoundations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name");
-            string myName = Console.ReadLine();
-            StringBuilder newName = new StringBuilder(myName);
-            newName.Insert(0,"Hello ");
-            Console.WriteLine(newName);
- 
+            int? bonus = null;
+            bool hasBonus = false;
+
+            hasBonus = (bonus.HasValue) ? true : false;
         }
     }
 }
