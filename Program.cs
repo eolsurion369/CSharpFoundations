d3c48426bb88ece944cb023b926be262c9d08f15
Unknown <eolsurion369@hotmail.com>
2017-11-20 10:06:08 -0800

    C# Exercise 7

--- Program.cs
+++ Program.cs
@@ -15,10 +15,23 @@ namespace CSharpFoundations
     {
         static void Main(string[] args)
         {
-            String[] Names = new String[] {"Sansa", "Bran", "Eddard", "Arya"};
-            foreach (String item in Names) {
-                Console.WriteLine(item);
-            }
+            MyMath mathInstance = new MyMath();
+            int firstSet = mathInstance.MultiplyTwoNumbers(2,4);
+            int secondSet = MultiplyTwoNumbers(16,12);
+            int finalResult = (firstSet * secondSet);
+            Console.WriteLine(finalResult.ToString());
         }
+
+        static int MultiplyTwoNumbers(int number1, int number2) {
+            int myresult = (number1 * number2);
+            return myresult;
+        }
+    }
+
+    class MyMath {
+        public int MultiplyTwoNumbers(int number1, int number2) {
+            int myresult = (number1 * number2);
+            return myresult;       
+        } 
     }
 }
